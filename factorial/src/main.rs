f997d03a3211aefcc7e59477f66eb04d2b85fe3e
James Elmore <email@jameselmore.org>
2023-12-29 11:11:48 -0500

    Finished factorial

--- factorial/src/main.rs
+++ factorial/src/main.rs
@@ -1,43 +1,64 @@
 // Problem 1: Write a program which finds the factorial of a number entered by the user. (check for all conditions).
 
 use std::io;
-fn main() {
-    // take user input
-    let mut buffer = String::new();
-    match io::stdin().read_line(&mut buffer) {
-        Ok(_) => {},
-        Err(error) => { println!("{error}"); }
-    }
-    // TODO coerce into u64 or fail
 
-    // TODO print
+
+fn main() {
+    let given = take_user_input();
+    match factorial(given) {
+        Some(output) => { println!("{given}! = {output}"); },
+        None => { println!("Output too large")}
+    }
 }
 
 // Naive implementation
-fn factorial(x: u128) -> u128 {
+fn factorial(x: u32) -> Option<u128> {
     if x == 0 {
-        return 1
+        return Some(1)
     }
     
-    let mut product = 1;
-    for i in 1..=x {
-        product *= i
+    let mut product = 1u128;
+    for i in 1u128..=x.into() {
+        product = match product.checked_mul(i) {
+            Some(it) => it,
+            None => return None,
+        }
     }
-
-    product
+    Some(product)
 }
 
+fn take_user_input() -> u32 {
+    const BUFFER_CAPACITY: usize = 32;
+    let mut buffer = String::with_capacity(BUFFER_CAPACITY); 
 
+    loop { //Loop until correct input is given
+        println!("Enter positive integer");
+        // io Error handling
+        match io::stdin().read_line(&mut buffer) {
+            Ok(_) => {},
+            Err(error) => {
+                buffer = String::with_capacity(BUFFER_CAPACITY);
+                println!("Error: {}\n Please try again", error)
+            }
+        }
+
+        // Parsing
+        match buffer.trim().parse::<u32>() {
+            Ok(given) => { return given }
+            Err(_) => { println!("Input must be a positive integer. Try again.")}
+        }
+    }
+} 
 
 #[cfg(test)]
 mod factorial_tests {
     use super::*;
 
     #[test]
-    fn factorial_0() { assert_eq!(factorial(0), 1); }
+    fn factorial_0() { assert_eq!(factorial(0), Some(1)); }
 
     #[test]
-    fn factorial_1() { assert_eq!(factorial(1), 1);}
+    fn factorial_1() { assert_eq!(factorial(1), Some(1));}
     
     #[test]
     fn factorial_n() {
@@ -49,7 +70,12 @@ mod factorial_tests {
             2 * 3 * 4 * 5
         ];
         for (input, expected_output) in inputs.into_iter().zip(factorials.into_iter()) {
-            assert_eq!(factorial(input), expected_output);
+            assert_eq!(factorial(input), Some(expected_output));
         }
+    }
+
+    #[test]
+    fn overflow_test() {
+        assert_eq!(factorial(99), None)
     }
 }
